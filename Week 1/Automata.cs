1187abba98ca1f20275f2832eb425d4ec737813f
Mauro de Lyon <maurodelyon@gmail.com>
2017-04-18 16:38:38 +0200

    init

---
+++ Week 1/Automata.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_1
+{
+    class Automata
+    {
+        // Or use a Map structure
+        private HashSet<Transition> transitions;
+
+        private SortedSet<Transition> states;
+        private SortedSet<Transition> startStates;
+        private SortedSet<Transition> finalStates;
+        private SortedSet<char> symbols;
+
+        public Automata() : this(new SortedSet<char>())
+        {
+        }
+
+        public Automata(char[] s) : this(new SortedSet<char>(s.ToList()))
+        {
+        }
+
+        public Automata(SortedSet<char> symbols)
+        {
+            transitions = new HashSet<Transition>();
+            states = new SortedSet<Transition>();
+            startStates = new SortedSet<Transition>();
+            finalStates = new SortedSet<Transition>();
+            this.setAlphabet(symbols);
+        }
+
+        public void setAlphabet(char[] s)
+        {
+            this.setAlphabet(new SortedSet<char>(s.ToList()));
+        }
+
+        public void setAlphabet(SortedSet<char> symbols)
+        {
+            this.symbols = symbols;
+        }
+
+        public SortedSet<char> getAlphabet()
+        {
+            return symbols;
+        }
+
+        public void addTransition(Transition t)
+        {
+            transitions.Add(t);
+            states.Add(t.getFromState());
+            states.Add(t.getToState());
+        }
+
+        public void defineAsStartState(Transition t)
+        {
+            // if already in states no problem because a Set will remove duplicates.
+            states.Add(t);
+            startStates.Add(t);
+        }
+
+        public void defineAsFinalState(Transition t)
+        {
+            // if already in states no problem because a Set will remove duplicates.
+            states.Add(t);
+            finalStates.Add(t);
+        }
+
+        public void printTransitions()
+        {
+            foreach (Transition transition in transitions)
+            {
+                Console.WriteLine(transition);
+            }
+        }
+
+        /*public bool isDFA()
+        {
+            bool isDFA = true;
+
+            foreach (Transition from in states)
+            {
+                foreach (char symbol in symbols)
+                {
+                    isDFA = isDFA && GetToStates(from, symbol);
+                }
+            }
+
+            return isDFA;
+        }
+
+        private bool GetToStates(Transition from, char symbol)
+        {
+            if (states.Select(e => e.getFromState().Equals(from.getFromState())).Select(e => e.getSymbol() == symbol).Count() == 1)
+            {
+                return true;
+            }
+            return false;
+        }*/
+    }
+}
---
+++ Week 1/Transition.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_1
+{
+    class Transition : IComparable<Transition>
+    {
+        public const char EPSILON = '$';
+
+        private Transition fromState;
+        private char symbol;
+        private Transition toState;
+
+        // this constructor can be used to define loops:
+        public Transition(Transition fromOrTo, char s) : this(fromOrTo, s, fromOrTo)
+        {
+        }
+
+        public Transition(Transition from, Transition to) : this(from, EPSILON, to)
+        {
+        }
+
+        public Transition(Transition from, char s, Transition to)
+        {
+            this.fromState = from;
+            this.symbol = s;
+            this.toState = to;
+        }
+
+        public int CompareTo(Transition transition)
+        {
+            int fromCmp = fromState.CompareTo(transition.fromState);
+            int symbolCmp = symbol.CompareTo(transition.symbol);
+            int toCmp = toState.CompareTo(transition.toState);
+
+            return (fromCmp != 0 ? fromCmp : (symbolCmp != 0 ? symbolCmp : toCmp));
+        }
+
+        public override bool Equals(object other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            else if (other is Transition)
+            {
+                return this.fromState.Equals(((Transition) other).fromState)
+                       && this.toState.Equals(((Transition) other).toState)
+                       && this.symbol == (((Transition) other).symbol);
+            }
+            else return false;
+        }
+
+
+        public Transition getFromState()
+        {
+            return fromState;
+        }
+
+        public Transition getToState()
+        {
+            return toState;
+        }
+
+        public char getSymbol()
+        {
+            return symbol;
+        }
+
+        public String toString()
+        {
+            return "(" + this.getFromState() + ", " + this.getSymbol() + ")" + "-->" + this.getToState();
+        }
+    }
+}
