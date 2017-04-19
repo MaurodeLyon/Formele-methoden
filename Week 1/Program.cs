91b3254f8016c1bf3a3d626c3ebf30dac4c93a4e
Arthur Brink <arthur.brink1@gmail.com>
2017-04-19 13:44:33 +0200

    First implementation

--- Week 1/Program.cs
+++ Week 1/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using GraphVizWrapper;
+using GraphVizWrapper.Commands;
+using GraphVizWrapper.Queries;
+
 
 namespace Week_1
 {
@@ -8,6 +12,21 @@ namespace Week_1
         {
             TestAutomata.Example3Week1().printTransitions();
             Console.WriteLine(TestAutomata.Example3Week1().IsDfa());
+
+            var getStartProcessQuery = new GetStartProcessQuery();
+            var getProcessStartInfoQuery = new GetProcessStartInfoQuery();
+            var registerLayoutPluginCommand = new RegisterLayoutPluginCommand(getProcessStartInfoQuery, getStartProcessQuery);
+
+            var wrapper = new GraphGeneration(getStartProcessQuery,
+                                  getProcessStartInfoQuery,
+                                  registerLayoutPluginCommand);
+
+            byte[] output = wrapper.GenerateGraph("digraph{a -> b; b -> c; c -> a;}", Enums.GraphReturnType.Jpg);
+            System.IO.File.WriteAllBytes("test22.jpg", output);
+
+            GraphVizParser.printGraph(TestAutomata.ExampleSlide14Lesson2(), "ExampleSlide14Lesson2");
+
+
             Console.ReadKey();
         }
     }
