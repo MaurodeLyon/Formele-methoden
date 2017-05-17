766cdf861e12c6a18ce67f6a8afa3affd4375544
Mauro de Lyon <maurodelyon@gmail.com>
2017-05-17 13:59:39 +0200

    Test automata

--- Formele methoden tests/TestAutomata.cs
+++ Formele methoden tests/TestAutomata.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Week_1;
 
 namespace Formele_methoden_tests
@@ -8,7 +9,7 @@ namespace Formele_methoden_tests
     {
         public static Automata<string> ExampleSlide8Lesson2()
         {
-            char[] alphabet = { 'a', 'b' };
+            char[] alphabet = {'a', 'b'};
             Automata<string> m = new Automata<string>(alphabet);
 
             m.AddTransition(new Transition<string>("q0", 'a', "q1"));
@@ -36,9 +37,10 @@ namespace Formele_methoden_tests
 
             return m;
         }
+
         public static Automata<string> ExampleSlide14Lesson2()
         {
-            char[] alphabet = { 'a', 'b' };
+            char[] alphabet = {'a', 'b'};
             Automata<string> m = new Automata<string>(alphabet);
 
             m.AddTransition(new Transition<string>("A", 'a', "C"));
@@ -71,7 +73,52 @@ namespace Formele_methoden_tests
         [TestMethod]
         public void Test()
         {
+            char[] alphabet = {'a', 'b'};
+            Automata<string> m = new Automata<string>(alphabet);
+
+            m.AddTransition(new Transition<string>("q0", 'a', "q1"));
+            m.AddTransition(new Transition<string>("q0", 'b', "q4"));
+
+            m.AddTransition(new Transition<string>("q1", 'a', "q4"));
+            m.AddTransition(new Transition<string>("q1", 'b', "q2"));
+
+            m.AddTransition(new Transition<string>("q2", 'a', "q3"));
+            m.AddTransition(new Transition<string>("q2", 'b', "q4"));
+
+            m.AddTransition(new Transition<string>("q3", 'a', "q1"));
+            m.AddTransition(new Transition<string>("q3", 'b', "q2"));
+
+            // the error state, loops for a and b:
+            m.AddTransition(new Transition<string>("q4", 'a'));
+            m.AddTransition(new Transition<string>("q4", 'b'));
+
+            // only on start state in a dfa:
+            m.DefineAsStartState("q0");
+
+            // two final states:
+            m.DefineAsFinalState("q2");
+            m.DefineAsFinalState("q3");
+
             Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void SimpleNdfa()
+        {
+            char[] alphabet = {'a', 'b'};
+            Automata<string> ndfa = new Automata<string>(alphabet);
+            ndfa.AddTransition(new Transition<string>("q0", 'a', "q1"));
+            ndfa.DefineAsStartState("q0");
+            ndfa.DefineAsFinalState("q1");
+
+            Assert.AreEqual(1, ndfa.FinalStates.Count);
+            Assert.AreEqual(1, ndfa.StartStates.Count);
+            Assert.AreEqual(2, ndfa.States.Count);
+            Assert.AreEqual(2, ndfa.Symbols.Count);
+            Assert.AreEqual(1, ndfa.Transitions.Count);
+
+            Assert.AreEqual("q0",ndfa.StartStates.ToList()[0]);
+            Assert.AreEqual("q1",ndfa.FinalStates.ToList()[0]);
         }
     }
 }
