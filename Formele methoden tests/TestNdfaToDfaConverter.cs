f7a85fd5c2cb268bc148a5cb402e3be61c6409f9
Mauro de Lyon <maurodelyon@gmail.com>
2017-05-24 11:04:08 +0200

    update to refactor

--- Formele methoden tests/TestNdfaToDfaConverter.cs
+++ Formele methoden tests/TestNdfaToDfaConverter.cs
@@ -1,7 +1,14 @@
-﻿namespace Formele_methoden_tests
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Formele_methoden_tests
 {
+    [TestClass]
     public class TestNdfaToDfaConverter
     {
-        
+        [TestMethod]
+        public void ConverterTest()
+        {
+            
+        }
     }
 }
