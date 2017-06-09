de1c908e64614350bc17aa6d32c1be4f32b0d738
Mauro de Lyon <maurodelyon@gmail.com>
2017-06-09 12:48:01 +0200

    added testapplication

--- Formele methoden/Program.cs
+++ Formele methoden/Program.cs
@@ -7,161 +7,88 @@ namespace Week_1
     {
         static void Main()
         {
-            RegExp reg = new RegExp("a").Or(new RegExp("b")).Dot(new RegExp("c").Or(new RegExp("d")));
-            Automaat<string> regExp = ThompsonConstruction.ConvertRegExp(reg);
-            GraphVizParser.PrintGraph(regExp, "ThompsonNdfa");
-
-            char[] alphabet = {'a', 'b'};
-            Automaat<string> dfa = new Automaat<string>(alphabet);
-            dfa.AddTransition(new Transition<string>("1", 'a', "2"));
-            dfa.AddTransition(new Transition<string>("1", 'b', "2"));
-            dfa.AddTransition(new Transition<string>("1", 'b', "3"));
-
-            dfa.AddTransition(new Transition<string>("2", 'a', "4"));
-            dfa.AddTransition(new Transition<string>("2", 'a', "2"));
-            
-            dfa.AddTransition(new Transition<string>("4", 'b', "3"));
-            dfa.AddTransition(new Transition<string>("4", 'b', "1"));
-
-            dfa.DefineAsStartState("1");
-            dfa.DefineAsFinalState("1");
-            dfa.DefineAsFinalState("4");
-
-            //Automaat<string> answer = NdfaToDfaConverter.Convert(dfa);
-            //GraphVizParser.PrintGraph(answer, "ndfa2dfa");
-
-            ////Automaat<string> ndfa = NdfaToDfaConverter.reverse(answer);
-
-
-            Automaat<string> dfaToMinimize = new Automaat<string>(alphabet);
-
-            dfaToMinimize.AddTransition(new Transition<string>("0", 'a', "2"));
-            dfaToMinimize.AddTransition(new Transition<string>("0", 'b', "3"));
-
-            dfaToMinimize.AddTransition(new Transition<string>("1", 'a', "3"));
-            dfaToMinimize.AddTransition(new Transition<string>("1", 'b', "2"));
-
-            dfaToMinimize.AddTransition(new Transition<string>("2", 'a', "0"));
-            dfaToMinimize.AddTransition(new Transition<string>("2", 'b', "4"));
-
-            dfaToMinimize.AddTransition(new Transition<string>("3", 'a', "1"));
-            dfaToMinimize.AddTransition(new Transition<string>("3", 'b', "5"));
-
-            dfaToMinimize.AddTransition(new Transition<string>("4", 'a', "6"));
-            dfaToMinimize.AddTransition(new Transition<string>("4", 'b', "5"));
-
-            dfaToMinimize.AddTransition(new Transition<string>("5", 'a', "2"));
-            dfaToMinimize.AddTransition(new Transition<string>("5", 'b', "0"));
-
-            dfaToMinimize.AddTransition(new Transition<string>("6", 'a', "4"));
-            dfaToMinimize.AddTransition(new Transition<string>("6", 'b', "0"));
-
-
-            dfaToMinimize.DefineAsStartState("0");
-            dfaToMinimize.DefineAsFinalState("1");
-            dfaToMinimize.DefineAsFinalState("3");
-            dfaToMinimize.DefineAsFinalState("4");
-            dfaToMinimize.DefineAsFinalState("6");
-
-            //Automaat<string> minimizedDfa = NdfaToDfaConverter.Convert(NdfaToDfaConverter.reverse(NdfaToDfaConverter.Convert(NdfaToDfaConverter.reverse(dfaToMinimize))));
-            //Automaat<string> minimizedDfa = NdfaToDfaConverter.Convert(NdfaToDfaConverter.Reverse(dfaToMinimize));
-            //GraphVizParser.PrintGraph(minimizedDfa,"minimizedDFA");
-
-            Automaat<string> epsilonNDFA = new Automaat<string>(alphabet);
-
-            epsilonNDFA.AddTransition(new Transition<string>("0", '$', "1"));
-            epsilonNDFA.AddTransition(new Transition<string>("0", '$', "7"));
-
-            epsilonNDFA.AddTransition(new Transition<string>("1", '$', "2"));
-            epsilonNDFA.AddTransition(new Transition<string>("1", '$', "4"));
-
-            epsilonNDFA.AddTransition(new Transition<string>("2", 'a', "3"));
-
-            epsilonNDFA.AddTransition(new Transition<string>("3", '$', "6"));
-
-            epsilonNDFA.AddTransition(new Transition<string>("4", 'b', "5"));
-
-            epsilonNDFA.AddTransition(new Transition<string>("5", '$', "6"));
-
-            epsilonNDFA.AddTransition(new Transition<string>("6", '$', "7"));
-            epsilonNDFA.AddTransition(new Transition<string>("6", '$', "1"));
-
-            epsilonNDFA.AddTransition(new Transition<string>("7", 'a', "8"));
-
-            epsilonNDFA.AddTransition(new Transition<string>("8", 'b', "9"));
-
-            epsilonNDFA.AddTransition(new Transition<string>("9", 'b', "10"));
-
-            epsilonNDFA.DefineAsStartState("0");
-            epsilonNDFA.DefineAsFinalState("10");
-
-            Automaat<string> epsilonDFA = NdfaToDfaConverter.Convert(epsilonNDFA);
-            GraphVizParser.PrintGraph(epsilonDFA, "epsilondfa");
-
-            Automaat<string> L1 = new Automaat<string>(alphabet);
-
-            L1.AddTransition(new Transition<string>("1", 'a', "2"));
-            L1.AddTransition(new Transition<string>("1", 'b', "1"));
-
-            L1.AddTransition(new Transition<string>("2", 'a', "2"));
-            L1.AddTransition(new Transition<string>("2", 'b', "3"));
-
-            L1.AddTransition(new Transition<string>("3", 'a', "4"));
-            L1.AddTransition(new Transition<string>("3", 'b', "1"));
-
-            L1.AddTransition(new Transition<string>("4", 'a', "4"));
-            L1.AddTransition(new Transition<string>("4", 'b', "4"));
-
-            L1.DefineAsStartState("1");
-            L1.DefineAsFinalState("4");
-
-            Automaat<string> L2 = new Automaat<string>(alphabet);
-
-            L2.AddTransition(new Transition<string>("1", 'a', "1"));
-            L2.AddTransition(new Transition<string>("1", 'b', "2"));
-
-            L2.AddTransition(new Transition<string>("2", 'a', "3"));
-            L2.AddTransition(new Transition<string>("2", 'b', "2"));
-
-            L2.AddTransition(new Transition<string>("3", 'a', "1"));
-            L2.AddTransition(new Transition<string>("3", 'b', "4"));
-
-            L2.AddTransition(new Transition<string>("4", 'a', "3"));
-            L2.AddTransition(new Transition<string>("4", 'b', "2"));
-
-            L2.DefineAsStartState("1");
-            L2.DefineAsFinalState("1");
-            L2.DefineAsFinalState("2");
-            L2.DefineAsFinalState("3");
-
-
-            Automaat<string> L3 = Automaat<string>.Union(L1, L2);
-            GraphVizParser.PrintGraph(L3, "L3");
-
-            RegExp regExpLeft = new RegExp("baa");
-            RegExp regExpRight = new RegExp("bb");
-            RegExp regExpOr = regExpLeft.Or(regExpRight);
-            Automaat<string> ndfaOr = ThompsonConstruction.ConvertRegExp(regExpOr);
-            GraphVizParser.PrintGraph(ndfaOr, "ndfaOr");
-            Automaat<string> dfaOr = NdfaToDfaConverter.Convert(ndfaOr);
-            GraphVizParser.PrintGraph(dfaOr, "dfaOr");
-
-
-            Automaat<string>.DfaGenerateValue test = new Automaat<string>.DfaGenerateValue { Parameter = "aab", IsNot = false, Type = Automaat<string>.GeneratorType.BeginsWith };
-            Automaat<string> gendfa = Automaat<string>.GenerateDfa(test,alphabet);
-            bool looping = true;
-            while (looping)
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine("Formele methoden test applicatie Mauro de Lyon & Arthur Brink");
+            Console.WriteLine("-------------------------------------------------------------");
+            while (true)
             {
-                string word = Console.ReadLine();
-                switch (word)
+                Console.WriteLine();
+                Console.WriteLine("Please select a regular expression:");
+                Console.WriteLine("1: a+");
+                Console.WriteLine("2: (a|b)*");
+                Console.WriteLine("3: (d|e)*.(a+.(b|c)+");
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                Console.WriteLine();
+                switch (keyInfo.KeyChar)
                 {
-                    case "quit":
-                        looping = false;
+                    case '1':
+                        Console.WriteLine("Option 1 selected : a+");
+                        Execute(new RegExp("a").Plus());
                         break;
-                    case "print":
+                    case '2':
+                        Console.WriteLine("Option 2 selected : (a|b)*");
+                        Execute(new RegExp("a").Or(new RegExp("b")).Star());
+                        break;
+                    case '3':
+                        Console.WriteLine("Option 3 selected : (d|e)*.(a+.(b|c)+");
+                        Execute(new RegExp("d").Or(new RegExp("e")).Star().Dot(new RegExp("a").Plus().Dot(new RegExp("b").Or(new RegExp("c")).Plus())));
+                        break;
+                    default:
+                        Console.WriteLine("incorrect key entered.");
                         break;
                 }
             }
+        }
+
+        public static void Execute(RegExp regExp)
+        {
+            // Convert regex to ndfa
+            Console.WriteLine();
+            Console.WriteLine("----- Converting regex to ndfa -----");
+            Automaat<string> ndfa = ThompsonConstruction.ConvertRegExp(regExp);
+            foreach (Transition<string> transition in ndfa.Transitions)
+            {
+                Console.WriteLine(transition.ToString());
+            }
+            Console.WriteLine("Printing image");
+            GraphVizParser.PrintGraph(ndfa, "ndfa");
+            Console.WriteLine("Image saved as ndfa.jpg");
+
+            // Convert ndfa to dfa
+            Console.WriteLine();
+            Console.WriteLine("----- Converting ndfa to dfa -----");
+            Automaat<string> dfa = NdfaToDfaConverter.Convert(ndfa);
+            foreach (Transition<string> transition in dfa.Transitions)
+            {
+                Console.WriteLine(transition.ToString());
+            }
+            Console.WriteLine("Printing image");
+            GraphVizParser.PrintGraph(dfa, "dfa");
+            Console.WriteLine("Image saved as dfa.jpg");
+
+            // Optimizing dfa Brzozowski
+            Console.WriteLine();
+            Console.WriteLine("----- Optimizing dfa: Brzozowski's algorithm -----");
+            Automaat<string> brzozowskiDfa = NdfaToDfaConverter.OptimizeDfa(dfa);
+            foreach (Transition<string> transition in brzozowskiDfa.Transitions)
+            {
+                Console.WriteLine(transition.ToString());
+            }
+            Console.WriteLine("Printing image");
+            GraphVizParser.PrintGraph(brzozowskiDfa, "BrzozowskiDfa");
+            Console.WriteLine("Image saved as BrzozowskiDfa.jpg");
+
+            // Optimize dfa Hopcroft
+            Console.WriteLine();
+            Console.WriteLine("----- Optimizing dfa: Hopcroft's algorithm -----");
+            Automaat<string> hopcroftDfa = HopcroftAlgorithm.MinimizeDfa(dfa);
+            foreach (Transition<string> transition in hopcroftDfa.Transitions)
+            {
+                Console.WriteLine(transition.ToString());
+            }
+            Console.WriteLine("Printing image");
+            GraphVizParser.PrintGraph(hopcroftDfa, "HopcroftDfa");
+            Console.WriteLine("Image saved as HopcroftDfa.jpg");
         }
     }
 }
